3170a5ab5db80496ca0ab095e77e475f9acaf414
ryoishim <54064063+ryoishim@users.noreply.github.com>
2019-10-06 20:44:16 +0900

    Update 04_upload_object.sh

--- 04_upload_object.sh
+++ 04_upload_object.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
 aws s3 sync ./image/ s3://ReplaceS3BucketName/
+aws s3api put-bucket-policy \
+  --bucket ReplaceS3BucketName \
+  --policy '{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AddPerm",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": "s3:GetObject",
+            "Resource": "arn:aws:s3:::ReplaceS3BucketName/*"
+        }
+    ]
+}'
