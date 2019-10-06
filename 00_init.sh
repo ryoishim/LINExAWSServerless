84452651acde570ea78219709818d852c9a7ba0b
EC2 Default User <ec2-user@ip-172-31-46-152.ap-northeast-1.compute.internal>
2019-10-06 08:21:55 +0000

    Update files and images.

--- 00_init.sh
+++ 00_init.sh
@@ -2,8 +2,8 @@
 
 echo -n "ユニークとなるバケット名を入力して下さい(ex: 20191106-ryoishim-handson):"
 read input
-sed -e "s/ReplaceS3BucketName/${input}/" ./03_create_bucket.py
-#sed -e "s/ReplaceS3BucketName/${input}/" ./04_upload_object.sh
-#sed -e "s/ReplaceS3BucketName/${input}/" ./05_index_faces.py
-#sed -e "s/ReplaceS3BucketName/${input}/" ./06_search_faces_by_image.py
-#sed -e "s/ReplaceS3BucketName/${input}/" ./function.py
+sed -i "s/ReplaceS3BucketName/${input}/" ./03_create_bucket.py
+sed -i "s/ReplaceS3BucketName/${input}/" ./04_upload_object.sh
+sed -i "s/ReplaceS3BucketName/${input}/" ./05_index_faces.py
+sed -i "s/ReplaceS3BucketName/${input}/" ./06_search_faces_by_image.py
+sed -i "s/ReplaceS3BucketName/${input}/" ./function.py
