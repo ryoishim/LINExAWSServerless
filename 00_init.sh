35198df8eede85ad9822b8c5f6ad4cb7e04b8331
EC2 Default User <ec2-user@ip-172-31-46-152.ap-northeast-1.compute.internal>
2019-10-06 05:59:03 +0000

    Update files and images.

--- 00_init.sh
+++ 00_init.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-echo "環境構築"
-echo "git cloneする"
+echo "■環境構築------------------------------------"
+pip install --upgrade pip
+sudo yum -y update
+sudo yum -y install python27 # Installs Python 2.7.
+sudo yum -y install python36 # Installs Python 3.6.
+curl -O https://bootstrap.pypa.io/get-pip.py # Get the install script.
+sudo python get-pip.py                       # Install pip.
+rm get-pip.py                                # Delete the install script.
+sudo python -m pip install boto3
+echo "■githubから必要な資材を取得する------------------"
+git clone https://github.com/ryoishim/LINExAWSServerless.git
