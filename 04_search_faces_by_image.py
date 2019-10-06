1754cf91380d2bf370d0ca47566e3dbf7c5e37d7
EC2 Default User <ec2-user@ip-172-31-46-152.ap-northeast-1.compute.internal>
2019-10-06 05:25:59 +0000

    First Commit.

---
+++ 04_search_faces_by_image.py
@@ -0,0 +1,30 @@
+#Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#PDX-License-Identifier: MIT-0 (For details, see https://github.com/awsdocs/amazon-rekognition-developer-guide/blob/master/LICENSE-SAMPLECODE.)
+
+import boto3
+
+if __name__ == "__main__":
+
+    bucket='rekognition-ryoishim'
+    collectionId='MyCollection'
+    fileName='input.jpg'
+    threshold = 1
+    maxFaces = 1
+
+    client=boto3.client('rekognition')
+
+  
+    response=client.search_faces_by_image(CollectionId=collectionId,
+                                Image={'S3Object':{'Bucket':bucket,'Name':fileName}},
+                                FaceMatchThreshold=threshold,
+                                MaxFaces=maxFaces)
+
+                                
+    faceMatches=response['FaceMatches']
+    print 'Matching faces'
+    for match in faceMatches:
+            print 'FileKey:' + match['Face']['ExternalImageId']
+            print 'FaceId:' + match['Face']['FaceId']
+            print 'Similarity: ' + "{:.2f}".format(match['Similarity']) + "%"
+            print
+
