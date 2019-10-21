f68ed318ba05d534fa3145ba7957c41baf7da6f0
EC2 Default User <ec2-user@ip-172-31-46-152.ap-northeast-1.compute.internal>
2019-10-21 23:44:53 +0000

    Update sqs triggered function.

--- put_message.py
+++ put_message.py
@@ -1,4 +1,6 @@
-import boto3, json
+from datetime import datetime
+import boto3
+import json
 import urllib
 import re
 import os
@@ -9,39 +11,45 @@ import hmac
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-
-from datetime import datetime
-
 logger.info('Loading function')
 
-## def Const var
 sqsClient = boto3.resource("sqs")
 queue = sqsClient.get_queue_by_name(QueueName='LINEMessage')
 channelSecret = os.environ['CHANNEL_ACCESS_TOKEN']
 
+def is_image_type_message(message, timestamp):
+    timestamp = timestamp
+    m_id = message['id']
+    m_type = message['type']
+    logger.info(f'timestamp: {timestamp}')
+    logger.info(f'message_id: {m_id}')
+    if (m_id == 'image'):
+        logger.info('Send message to SQS because MessageType is image.')
+        return True
+    else:
+        logger.info('MessageType is NOT image.')
+        return False
+
+def check_signature(event):
+    # SignatureVerification
+    text = str(event['body-json'])
+    xlinesignature = event['params']['header']['X-Line-Signature']
+    hash = hmac.new(channelSecret.encode('utf-8'),
+                    text.encode('utf-8'), hashlib.sha256).digest()
+    signature = base64.b64encode(hash)
+    logger.info(f'Line-Signature: {xlinesignature}')
+    logger.info(f'Body-Signature: {signature}')
+
 def lambda_handler(event, context):
 
     jsonstr = json.dumps(event, indent=2)
-    logger.info("Received event: " + jsonstr)
-
+    logger.info(f'Received event:: {jsonstr}')
 
     # SignatureVerification
-    hash = hmac.new(channelSecret.encode('utf-8'),
-        str(event['body-json']['events'][0]).encode('utf-8'), hashlib.sha256).digest()
-    signature = base64.b64encode(hash)
-    logger.info("Line-Signature: " + event['params']['header']['X-Line-Signature'])
-    logger.info("Body-Signature: " + str(signature))
+    check_signature(event)
 
     for e in event['body-json']['events']:
-    # e が画像のメッセージかどうか判別
-        timestamp = e['timestamp']
-        messageId = e['message']['id']
-        logger.info("timestamp: " + str(timestamp))
-        logger.info("messageId: " + str(messageId))
-        if (e['message']['type'] == "image"):
-            logger.info("Send message to SQS because MessageType is image.")
+        if is_image_type_message(e['message'], e['timestamp']):
             queue.send_message(MessageBody=json.dumps(e))
-        else:
-            logger.info("MessageType is NOT image.")
 
-    return 0    
+    return 0 
